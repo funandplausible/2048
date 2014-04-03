655572211dace9e2e7a4d6a7aaa785c632c3e7b4
Sam Phippen <samphippen@googlemail.com>
2014-04-03 19:19:04 +0100

    selfie version

--- js/sequencer_audio.js
+++ js/sequencer_audio.js
@@ -1,8 +1,62 @@
-function SequencerPlayer(tempo) {
-};
 
-SequencerPlayer.prototype.onTick = function(beatNumber, tileValue) {
-};
+// Trigger a function when each chunk of audio is played
+// Note that this may go out of sync if the user shifts focus away from the page.
+// You will need to supply your Echo Nest API key, the trackID, and a URL to the track.
+// The supplied track can be found in the audio subdirectory.
+var apiKey = 'NODV0LSHAP8SI9WAF';
+var trackID = 'TROSXDD1452892CA20';
+var trackURL = 'http://funandplausible.com/selfie.mp3'
 
-SequencerPlayer.prototype.onTempoChange(function(newTempo) {
+var remixer;
+var player;
+var track;
+var remixed;
+
+var beatCount = 0;
+
+function trigger() {
+    window.currentBeat += 1;
+    window.currentBeat = window.currentBeat % 16;
+    window.ping();
+}
+
+function init() {
+    if (window.webkitAudioContext === undefined) {
+        error("Sorry, this app needs advanced web audio. Your browser doesn't"
+            + " support it. Try the latest version of Chrome");
+    } else {
+        var context = new webkitAudioContext();
+        remixer = createJRemixer(context, $, apiKey);
+        var effects = [];
+        player = remixer.getPlayer(effects);
+
+        window.gain = effects[0];
+        $("#info").text("Loading analysis data...");
+
+        remixer.remixTrackById(trackID, trackURL, function(t, percent) {
+            track = t;
+
+            $("#info").text(percent + "% of the track loaded");
+            if (percent == 100) {
+                $("#info").text(percent + "% of the track loaded, remixing...");
+            }
+
+            if (track.status == 'ok') {
+                player.addOnPlayCallback(trigger);
+                $("#info").text("Remix complete!");
+                $("#info").hide();
+                window.gameManager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+                window.gameManager.restart();
+                window.ping();
+            }
+        });
+    }
+}
+
+window.onload = init;
+$(document).keyup(function(e) {
+    if (e.keyCode == 32) {
+        player.stop();
+    }
 });
+
--- js/sequencer_events.js
+++ js/sequencer_events.js
@@ -1,32 +1,29 @@
 (function() {
-    var BPM = 120
-    var currentBeat = 4;
     var increment = 1;
-    $(document).keyup(function(e) {
-        if (e.keyCode == 32) {
-            increment = 1-increment;
+    window.currentBeat = 0;
+    console.log("defining ping");
+    window.ping = function() {
+        var currentRow  = Math.floor(window.currentBeat / 4) + 1;
+        var currentTile = window.currentBeat % 4 + 1;
+        var elem = $(".tile-position-" + currentTile + "-" + currentRow + " .tile-inner")[0];
+        $(".tile-highlight").addClass("tile-highlight-out");
+        $(".tile-highlight").removeClass("tile-highlight");
+        console.log(currentRow + " " + currentTile);
+
+        if (elem != null) {
+            var elem = $(".tile-position-" + currentTile + "-" + currentRow + " .tile-inner");
+            var tileValue = parseInt(elem.text());
+            var beat = track.analysis.beats[((window.currentBeat+1) + (tileValue*4)) % track.analysis.beats.length];
+            $(elem).addClass("tile-highlight");
+            player.queue(beat);
+            window.gain.gain.value = 1.0;
+        } else {
+            var elem = $(".grid-container .grid-row:nth-child(" + currentRow + ") .grid-cell:nth-child(" + currentTile + ")");
+            elem.addClass("tile-highlight");
+            player.queue(track.analysis.beats[window.currentBeat+1]);
+            window.gain.gain.value = 0.5;
         }
-    });
-    $(document).ready(function() {
-        setInterval(function() {
-            var currentRow  = Math.floor(currentBeat / 4) + 1;
-            var currentTile = currentBeat % 4 + 1;
-            var elem = $(".tile-position-" + currentTile + "-" + currentRow + " .tile-inner")[0];
-            $(".tile-highlight").addClass("tile-highlight-out");
-            $(".tile-highlight").removeClass("tile-highlight");
-            console.log(currentRow + " " + currentTile);
 
-            if (elem != null) {
-                var elem = $(".tile-position-" + currentTile + "-" + currentRow + " .tile-inner");
-                $(elem).addClass("tile-highlight");
-            } else {
-                var elem = $(".grid-container .grid-row:nth-child(" + currentRow + ") .grid-cell:nth-child(" + currentTile + ")");
-                elem.addClass("tile-highlight");
-            }
-
-            currentBeat += increment;
-            currentBeat = currentBeat % 16;
-
-        }, (1000*60.0)/BPM);
-    });
+        window.currentBeat = window.currentBeat % 16;
+    }
 })();
